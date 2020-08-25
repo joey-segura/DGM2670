954c9487f068dd31cdcad008e36d8a9d1e263a46
Joey Segura <10735119@my.uvu.edu>
2020-08-25 17:39:01 -0600

    BeanMovement

---
+++ DGM2670_GAME/Assets/ThirdPersonMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMove : MonoBehaviour
+{
+    public CharacterController cntrl;
+    public Transform cam;
+
+    public float spd = 6f;
+    public float smoothTime = 0.1f;
+    private float turnVelocity;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z)*Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, smoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            cntrl.Move(moveDirection.normalized * spd * Time.deltaTime);
+        }
+    }
+}
