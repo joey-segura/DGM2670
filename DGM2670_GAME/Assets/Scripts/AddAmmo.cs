f5766d0ef17940161b643701de6d965c14980d05
Joey Segura <10735119@my.uvu.edu>
2020-08-26 14:37:32 -0600

    AmmoPIckup | Destroys ammo on pickup and adds value but won't display.

---
+++ DGM2670_GAME/Assets/Scripts/AddAmmo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddAmmo : MonoBehaviour
+{
+    public IntData ammoValue;
+    
+    void OnTriggerEnter(Collider col)
+    {
+        ammoValue.value++;
+        
+        if (col.gameObject.name == "Player")
+        {
+            Destroy(this.gameObject); 
+        }
+    }
+}
---
+++ DGM2670_GAME/Assets/Scripts/UpadateAmmo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateAmmo : MonoBehaviour
+{
+        public IntData ammoValue;
+        private Text text;
+
+        private void Start()
+        {
+                text = GetComponent<Text>();
+        }
+        
+        private void Update()
+        {
+                text.text = ammoValue.value.ToString();
+        }
+}
