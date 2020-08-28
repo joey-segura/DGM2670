d36b3ae7c2872ac4756bf38d37015779904bbb16
Joey Segura <10735119@my.uvu.edu>
2020-08-27 20:04:50 -0600

    LookAtMouse

--- DGM2670_GAME/Assets/Scripts/ThirdPersonMove.cs
+++ DGM2670_GAME/Assets/Scripts/ThirdPersonMove.cs
@@ -6,22 +6,37 @@ public class ThirdPersonMove : MonoBehaviour
 {
     public CharacterController cntrl;
     public Transform cam;
+    public Camera cam2;
 
-    public float spd = 6f;
+    public float spd = 8f;
     public float smoothTime = 0.1f;
     private float turnVelocity;
     
     void Start()
     {
-        
+        cam2 = FindObjectOfType<Camera>();
     }
 
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
 
+        //Look at mouse position
+        Ray cameraRay = cam2.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 focusPoint = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, focusPoint, Color.red);
+            transform.LookAt(focusPoint);
+        }
+        
+        
+       
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z)*Mathf.Rad2Deg + cam.eulerAngles.y;
