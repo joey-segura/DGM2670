f9e05d14d8b81b39d577c7c2d257887ffc13d194
Joey Segura <10735119@my.uvu.edu>
2020-08-29 18:35:13 -0600

    instantiate Barrier Wall

---
+++ DGM2670_GAME/Assets/Scripts/shootGun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+public class shootGun : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject playerWall;
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            SpawnWall();
+        }
+    }
+
+    void SpawnWall()
+    {
+        GameObject newWall =Instantiate(playerWall, spawnPoint.position, spawnPoint.rotation);
+        Rigidbody rb = newWall.GetComponent<Rigidbody>();
+        Destroy(newWall, 3.0f);
+    }
+}
