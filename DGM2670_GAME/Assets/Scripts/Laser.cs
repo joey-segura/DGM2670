7ed507cb8d152c4e02670855c81fd72056df4641
Joey Segura <10735119@my.uvu.edu>
2020-09-18 11:29:56 -0600

    Enemy Laser (Won't Update)

---
+++ DGM2670_GAME/Assets/Scripts/Laser.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+    public Vector3 playerLocation;
+    private LineRenderer laser;
+    
+    void Start()
+    {
+        playerLocation = GameObject.Find("Player").transform.position;
+        laser = GetComponent<LineRenderer>();
+    }
+    
+    void Update()
+    {
+        Vector3 origin = this.gameObject.transform.position;
+        Vector3 direction = transform.forward;
+        
+        Debug.DrawRay(origin, direction * 10f, Color.red);
+
+        RaycastHit hit;
+        Ray gunRay = new Ray(origin, direction);
+        laser.SetPosition(0, origin);
+
+        if (Physics.Raycast(gunRay, out hit))
+        {
+            hit.point = playerLocation;
+            Debug.Log(hit.point);
+            laser.SetPosition(1, hit.point);
+        }
+        
+        transform.LookAt(hit.point);
+    }
+}
