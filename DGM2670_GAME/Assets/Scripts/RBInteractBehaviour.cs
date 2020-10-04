c8ce7238c533c059e4d42f3074f6cb29237647fe
Joey Segura <10735119@my.uvu.edu>
2020-10-03 22:00:57 -0600

    CharacterController Interact with Rbodies

---
+++ DGM2670_GAME/Assets/Scripts/RBInteractBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RBInteractBehaviour : MonoBehaviour
+{
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody rbody = hit.collider.attachedRigidbody;
+        if (rbody != null && !rbody.isKinematic)
+        {
+            rbody.velocity += hit.controller.velocity;
+        }
+    }
+}
