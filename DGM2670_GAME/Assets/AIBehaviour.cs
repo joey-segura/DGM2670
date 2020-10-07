fef7b4831d95fec167ad97e12585790fa45abfc1
Joey Segura <10735119@my.uvu.edu>
2020-10-07 14:17:43 -0600

    AI

---
+++ DGM2670_GAME/Assets/AIBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.XR.WSA.Input;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIBehaviour : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+
+    public Transform player;
+
+    public bool canNavigate;
+    
+    private WaitForFixedUpdate wffu;
+   
+    public float holdTime = 1f;
+    
+    private WaitForSeconds wfs;
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        wfs = new WaitForSeconds(holdTime);
+    }
+    
+    private IEnumerator Navigate()
+    {
+        canNavigate = true;
+
+        while (canNavigate)
+        {
+            yield return wffu;
+            agent.destination = player.position;
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        canNavigate = false;
+        StartCoroutine(Navigate());
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        canNavigate = false;
+        agent.destination = this.transform.position;
+    }
+}
