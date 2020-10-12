d9212fea63aa5e7e69913dff6b1c11ca50e952c8
Joey Segura <10735119@my.uvu.edu>
2020-10-12 14:00:33 -0600

    ChainChomp_Charge

--- DGM2670_GAME/Assets/AIBehaviour.cs
+++ DGM2670_GAME/Assets/AIBehaviour.cs
@@ -9,29 +9,31 @@ public class AIBehaviour : MonoBehaviour
 {
 
     private NavMeshAgent agent;
-
     public GameObject playerLocation;
+    public float chargeCooldown = 2f, timeSinceCharge;
+    public bool isCharging;
 
-    public bool canNavigate;
-    
-    private WaitForFixedUpdate wffu;
-   
-    public float holdTime = 1f;
-    
-    private WaitForSeconds wfs;
-    
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         playerLocation = GameObject.Find("Humanoid_01");
-        wfs = new WaitForSeconds(holdTime);
+        isCharging = false;
+    }
+
+    void Update()
+    {
+        timeSinceCharge += Time.deltaTime;
     }
     
     //Add patrol points so the crab won't charge again until he has reached the most recent point that was dropped. (Point drops ontriggerenter.)
     
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        agent.destination = playerLocation.transform.position;
-        agent.speed = 2f;
+        if (timeSinceCharge > chargeCooldown)
+        {
+            agent.destination = playerLocation.transform.position;
+            agent.speed = 25;
+            timeSinceCharge = 0;
+        }
     }
 }
