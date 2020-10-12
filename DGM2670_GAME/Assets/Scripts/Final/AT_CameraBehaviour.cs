82f1e9d11f49a2989e99030eb0785b41275b3bec
Joey Segura <10735119@my.uvu.edu>
2020-10-12 13:27:05 -0600

    CamerFOV

--- DGM2670_GAME/Assets/Scripts/Final/AT_CameraBehaviour.cs
+++ DGM2670_GAME/Assets/Scripts/Final/AT_CameraBehaviour.cs
@@ -6,20 +6,33 @@ public class AT_CameraBehaviour : MonoBehaviour
 {
     public GameObject cameraFocus;
     public float followSpeed = 0.1f;
+    public float cameraFOV;
 
 
     void Start()
     {
         cameraFocus = GameObject.Find("Humanoid_01");
+        cameraFOV = 60.0f;
     }
 
    
     void LateUpdate()
     {
+        Camera.main.fieldOfView = cameraFOV;
+        
         if (transform.position != cameraFocus.transform.position)
         {
             Vector3 playerPosition = new Vector3(cameraFocus.transform.position.x, cameraFocus.transform.position.y + 12f, cameraFocus.transform.position.z - 12f);
             transform.position = Vector3.Lerp(transform.position, playerPosition, followSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            cameraFOV = 65.0f;
+        }
+        else
+        {
+            cameraFOV = 60.0f;
         }
     }
 }
