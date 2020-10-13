452a3f6d7761234f1068fcf11ba6996990593889
Joey Segura <10735119@my.uvu.edu>
2020-10-12 20:37:56 -0600

    Patrol+Charge Merge (Broken)

--- DGM2670_GAME/Assets/AIBehaviour.cs
+++ DGM2670_GAME/Assets/AIBehaviour.cs
@@ -9,31 +9,69 @@ public class AIBehaviour : MonoBehaviour
 {
 
     private NavMeshAgent agent;
+    private Rigidbody agentRB;
     public GameObject playerLocation;
     public float chargeCooldown = 2f, timeSinceCharge;
-    public bool isCharging;
+    public bool targetInBounds = false;
+    
+    public List<GameObject> patrolPoints;
+    private int i = 0;
 
+    void Awake()
+    {
+        playerLocation = GameObject.FindWithTag("Player");
+    }
+    
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        playerLocation = GameObject.Find("Humanoid_01");
-        isCharging = false;
+        agentRB = GetComponent<Rigidbody>();
+        i = 0;
     }
 
     void Update()
     {
         timeSinceCharge += Time.deltaTime;
+        
+        if (targetInBounds == false)
+        {
+            if (agent.pathPending || !(agent.remainingDistance < 0.5f))
+            {
+                agent.speed = 3.5f;
+                return;
+            }
+            agent.destination = patrolPoints[i].transform.position;
+            i = (i + 1) % patrolPoints.Count;
+        }
+        else
+        {
+            agent.destination = playerLocation.transform.position;
+            agent.speed = 20;
+            timeSinceCharge = 0;
+        }
     }
+    
+  
     
     //Add patrol points so the crab won't charge again until he has reached the most recent point that was dropped. (Point drops ontriggerenter.)
     
     private void OnTriggerStay(Collider other)
     {
-        if (timeSinceCharge > chargeCooldown)
+        if (other.tag == "Player" && timeSinceCharge > chargeCooldown)
         {
-            agent.destination = playerLocation.transform.position;
-            agent.speed = 25;
-            timeSinceCharge = 0;
+            //agent.destination = playerLocation.transform.position;
+            //agent.speed = 25;
+            //timeSinceCharge = 0;
+            targetInBounds = true;
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        targetInBounds = false;
+    }
+    
+        
+        
+  
 }
---
+++ DGM2670_GAME/Assets/Scripts/Final/AIPatrol.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIPatrol : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    public List<GameObject> patrolPoints;
+    private int i = 0;
+   
+    void Start()
+    {
+        i = 0;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (agent.pathPending || !(agent.remainingDistance < 0.5f)) return;
+        agent.destination = patrolPoints[i].transform.position;
+        i = (i + 1) % patrolPoints.Count;
+    }
+}
