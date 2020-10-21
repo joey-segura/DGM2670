59dab26892232b50e059c6cfcb5ca5ac3ca2b65c
Joey Segura <10735119@my.uvu.edu>
2020-10-21 14:51:25 -0600

    State Machines

---
+++ DGM2670_GAME/Assets/Scripts/Behaviours/CharacterWithStates.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterWithStates : MonoBehaviour
+{
+    private CharacterController controller;
+    public enum characterStates
+    {
+        StandardWalk,
+        WallCrawl
+    }
+    
+    public characterStates currentCharacterState;
+    
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    
+    private void Update()
+    {
+        switch (currentCharacterState)
+        {
+            case characterStates.StandardWalk:
+                print("Standard Walk");
+                break;
+            case characterStates.WallCrawl:
+                print("Wall Crawl");
+                break;
+        }
+    }
+}
---
+++ DGM2670_GAME/Assets/Scripts/DataSO/CharacterStateMachineData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CharacterStateMachineData : ScriptableObject
+{
+    public enum characterStates
+    {
+        StandardWalk,
+        WallCrawl,
+        Idle
+    }
+
+    public characterStates value;
+}
