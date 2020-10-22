7bc5bfbe8b654d71b4634bad5a85dd29d046bad9
Joey Segura <10735119@my.uvu.edu>
2020-10-21 21:25:42 -0600

    LockWall(Visual)

---
+++ DGM2670_GAME/Assets/LockWallBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockWallBehaviour : MonoBehaviour
+{
+    //Four bools, once all are marked true the wall lowers using a LERP.
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
