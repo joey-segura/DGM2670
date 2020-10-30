c80e7c1cbe1d5c6070b08a324c693eb2be742520
Joey Segura <10735119@my.uvu.edu>
2020-10-29 22:38:31 -0600

    LightSwitch + Trolley Safely Introduced

---
+++ DGM2670_GAME/Assets/LightSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    private Light mainLight;
+    
+    void Start()
+    {
+        mainLight = GetComponent<Light>();
+
+        mainLight.intensity = 0.5f;
+    }
+}
