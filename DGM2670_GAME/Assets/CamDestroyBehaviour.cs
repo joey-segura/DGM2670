d81e11fdfbcc0c6aa338efa771031a9058b95417
Joey Segura <10735119@my.uvu.edu>
2020-11-14 23:01:38 -0700

    Light BreakApart (Not Working)

---
+++ DGM2670_GAME/Assets/CamDestroyBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamDestroyBehaviour : MonoBehaviour
+{
+
+    public GameObject secCam;
+
+    void OnTriggerEnter()
+    {
+        Debug.Log("Triggered");
+        Invoke("DetachChildren", 0);
+    }
+
+    public void DetachChildren(GameObject obj, bool isKin)
+    {
+        Rigidbody[] rBodies = gameObject.GetComponentsInChildren<Rigidbody>();
+        
+        for (int i=0; i < rBodies.Length; i++)
+        {
+            rBodies[i].isKinematic = true;
+        }
+    }
+}
