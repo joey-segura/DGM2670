a28617c6dc821e61dbf662215768a5f22b2b7da1
Joey Segura <10735119@my.uvu.edu>
2020-11-30 21:28:49 -0700

    BrokenCam UI (Only works for first cam.. )

---
+++ DGM2670_GAME/Assets/CamScoreBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CamScoreBehaviour : MonoBehaviour
+{
+    public Text camText;
+    public int camAmt;
+    
+    void Start()
+    {
+        camAmt = 0;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            camAmt = camAmt + 1;
+            camText.text = camAmt.ToString();
+            Destroy(this.gameObject);
+        }
+    }
+}
